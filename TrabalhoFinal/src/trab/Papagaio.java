9e8dbe9a24051d99b6716e923eadfed6ee22bf10
daniellberg <danielbergons@gmail.com>
2023-06-04 18:23:57 -0300

    implementa classe papagaio

--- TrabalhoFinal/src/trab/Papagaio.java
+++ TrabalhoFinal/src/trab/Papagaio.java
@@ -1,5 +1,11 @@
 package trab;
 
 public class Papagaio {
+    public void brinca() {
+        System.out.println("Brincando...");
+    }
 
+    public void fazNecessidades() {
+        System.out.println("Fazendo necessidades...");
+    }
 }
