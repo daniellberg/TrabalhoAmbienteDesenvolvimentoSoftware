0b36a657299eba75f0cf3890f8760afc41a12c2f
daniellberg <danielbergons@gmail.com>
2023-06-04 18:23:43 -0300

    implementa classe gato

--- TrabalhoFinal/src/trab/Gato.java
+++ TrabalhoFinal/src/trab/Gato.java
@@ -2,4 +2,13 @@ package trab;
 
 public class Gato {
 
+    public void fazSom(){
+        System.out.println("Emitindo barulho...");
+    }
+
+    public void dorme(){
+        System.out.println("Dormindo...");
+    }
+
+  //chama método de outra classe
 }
