1e523dfad952d3f9d78ec31a3fb8ec8e25e3db21
daniellberg <danielbergons@gmail.com>
2023-06-04 18:23:05 -0300

    implementa classe cachorro

--- TrabalhoFinal/src/trab/Cachorro.java
+++ TrabalhoFinal/src/trab/Cachorro.java
@@ -2,4 +2,18 @@ package trab;
 
 public class Cachorro {
 	
+
+
+    public void come(){
+        System.out.println("Comendo");
+    }
+
+    public void bebeAgua(){
+        System.out.println("Bebendo...");
+    }
+
+    // chama método de outras classes
+
+    
+    
 }
